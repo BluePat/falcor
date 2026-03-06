8146b3faeef75d0adb610901e650f21f5c6260c7
weareaisle-local-app <Iv23lijiANikYU33q0zF+weareaisle-local-app@users.noreply.github.com>
2026-03-06 02:31:48 +0000

    Apply fix for issue: Prototype Pollution via Dynamic Property Access

--- performance/models/legacy/_.js
+++ performance/models/legacy/_.js
@@ -5435,7 +5435,7 @@ function pathMapWithObserver(paths_, observer_, parent) {
                                 key = key.offset === void 0 && (key.offset = key.from || (key.from = 0)) || key.offset;
                             }
                         }
-                        if (key == null) {
+                        if (key == null || key === '__proto__' || key === 'constructor' || key === 'prototype') {
                             continue;
                         }
                         observers = (context = contextParent[key] || (contextParent[key] = {
@@ -5458,7 +5458,7 @@ function pathMapWithObserver(paths_, observer_, parent) {
                                 key = key.offset === void 0 && (key.offset = key.from || (key.from = 0)) || key.offset;
                             }
                         }
-                        if (key != null) {
+                        if (key != null && key !== '__proto__' && key !== 'constructor' && key !== 'prototype') {
                             observers = (context = contextParent[key] || (contextParent[key] = {
                                 __observers: []
                             })).__observers;
