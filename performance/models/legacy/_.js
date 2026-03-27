270ecbb42694223119e95626f6d82d20103752c3
weareaisle-local-app <Iv23lijiANikYU33q0zF+weareaisle-local-app@users.noreply.github.com>
2026-03-27 12:34:30 +0000

    Apply fix for issue: Prototype Pollution via Dynamic Property Access

--- performance/models/legacy/_.js
+++ performance/models/legacy/_.js
@@ -6002,7 +6002,10 @@ function serialize(cache) {
     return message;
 
     function internalKeys(x) {
-        return x[0] !== '_' || x[1] !== '_';
+        return (x[0] !== '_' || x[1] !== '_') && 
+               x !== 'constructor' && 
+               x !== 'prototype' && 
+               x !== '__proto__';
     }
 }
 
@@ -6040,7 +6043,11 @@ function deserialize(cache) {
     return this;
 
     function internalKeys(x) {
-        return x[0] !== '$' && (x[0] !== '_' || x[1] !== '_');
+        return x[0] !== '$' && 
+               (x[0] !== '_' || x[1] !== '_') && 
+               x !== 'constructor' && 
+               x !== 'prototype' && 
+               x !== '__proto__';
     }
 }
 
@@ -6131,7 +6138,12 @@ function createKey(list) {
 }
 
 function notPathMapInternalKeys(key) {
-    return key !== '__observers' && key !== '__pending' && key !== '__batchID';
+    return key !== '__observers' && 
+           key !== '__pending' && 
+           key !== '__batchID' && 
+           key !== 'constructor' && 
+           key !== 'prototype' && 
+           key !== '__proto__';
 }
 /**
  * Builds the set of collapsed
